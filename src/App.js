edeef92471347e6edff96aa7d310c0722eac349b
kalinin <prozaik81-2@yandex.ru>
2020-10-25 19:13:22 +0300

    submit validation form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Redirect, Link } from "react-router-dom";
 import Page1smart from './Components/Page1/Page1smart';
 import Page2smart from './Components/Page2/Page2smart';
 import Page3smart from './Components/Page3/Page3smart';
+import Page4smart from './Components/Page4/Page4smart';
 import Page404 from './Components/Page404/Page404';
 
 
@@ -16,6 +17,7 @@ function App() {
         <Link to="/page1">Simple form</Link>
         <Link to="/page2">Sync validation form</Link>        
         <Link to="/page3">Field level validation form</Link>        
+        <Link to="/page4">Submit validation form</Link>        
       </header>
       <br/>
       <br/>
@@ -25,6 +27,7 @@ function App() {
         <Route exact path='/page1'    render={ () => <Page1smart/> }/>
         <Route exact path='/page2'    render={ () => <Page2smart/> }/>
         <Route exact path='/page3'    render={ () => <Page3smart/> }/>
+        <Route exact path='/page4'    render={ () => <Page4smart/> }/>
         <Route exact path='/page404'  render={ () => <Page404/> }/>
         <Redirect to='/page404' />
       </main>
---
+++ src/Components/Page4/Page4smart.jsx
@@ -0,0 +1,12 @@
+import ReduxSubmitValidationForm from './Form';
+
+function Page4() {
+  return (
+    <div>
+      <h1>submit valid form example</h1>
+      <ReduxSubmitValidationForm/>
+    </div>
+  );
+}
+
+export default Page4;
