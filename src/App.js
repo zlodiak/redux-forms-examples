9fe0f35c4f4b8b21eb07ee36e92fef99232e7e22
kalinin <prozaik81-2@yandex.ru>
2020-10-27 23:25:33 +0300

    initialize via store

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import Page2smart from './Components/Page2/Page2smart';
 import Page3smart from './Components/Page3/Page3smart';
 import Page4smart from './Components/Page4/Page4smart';
 import Page5smart from './Components/Page5/Page5smart';
+import InitializeFromStateForm from './Components/Page6/Page6smart';
 import Page404 from './Components/Page404/Page404';
 
 
 import './App.css';
 
 function App() {
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+  async function showResults(values) {
+    await sleep(500); // simulate server latency
+    window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
+  };
+
   return (
     <BrowserRouter>
       <header>
@@ -20,6 +28,7 @@ function App() {
         <Link to="/page3">Field level validation form</Link>        
         <Link to="/page4">Submit validation form</Link>        
         <Link to="/page5">Async blur validation form</Link>        
+        <Link to="/page6">Initialize form</Link>        
       </header>
       <br/>
       <br/>
@@ -31,6 +40,7 @@ function App() {
         <Route exact path='/page3'    render={ () => <Page3smart/> }/>
         <Route exact path='/page4'    render={ () => <Page4smart/> }/>
         <Route exact path='/page5'    render={ () => <Page5smart/> }/>
+        <Route exact path='/page6'    render={ () => <InitializeFromStateForm onSubmit={showResults}/> }/>
         <Route exact path='/page404'  render={ () => <Page404/> }/>
         <Redirect to='/page404' />
       </main>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import {reducer as formReducer} from 'redux-form';
+import account from '../Components/Page6/account';
 
 import appReducer from './appReducer';
 
@@ -7,6 +8,7 @@ import appReducer from './appReducer';
 let reducers = combineReducers({
     form: formReducer,
     appReducer,
+    account,
 });
 
 const store = createStore(reducers);
